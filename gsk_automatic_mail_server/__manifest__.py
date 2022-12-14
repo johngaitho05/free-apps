0addc6b9e6248b3d497045295fa06e2abfe7d0e1
John Gaitho <johngaitho05@gmail.com>
2022-12-14 17:02:22 +0300

    automaic mail server

---
+++ gsk_automatic_mail_server/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Automatic Mail Servers',
+    'version': '16.0.1.0.0',
+    'summary': 'Choose the correct mail server automatically',
+    'category': 'Productivity/Discuss',
+    'author': 'Gritnec Solutions',
+    'website': 'https://gritnecsolutions.com',
+    'license': 'LGPL-3',
+    'depends': ['mail'],
+    'installable': True,
+    'auto_install': False,
+}
