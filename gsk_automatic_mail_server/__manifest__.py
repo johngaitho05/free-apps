8dd40037548c4c3e02876d7562ca2d4a00f30bd6
John Gaitho <johngaitho05@gmail.com>
2022-12-15 15:27:32 +0300

    update documentation

--- gsk_automatic_mail_server/__manifest__.py
+++ gsk_automatic_mail_server/__manifest__.py
@@ -2,6 +2,17 @@
     'name': 'Automatic Mail Servers',
     'version': '16.0.1.0.0',
     'summary': 'Choose the correct mail server automatically',
+    'description':"""
+        Automatic mail Server
+        SMTPServerDisconnected
+        please run connect() first
+        Zoho issue fix
+        Mail Delivery
+        Outgoing mail servers
+        Fix mail Delivery
+        Fix email issue
+        Send emails
+    """,
     'category': 'Productivity/Discuss',
     'author': 'Gritnec Solutions',
     'website': 'https://gritnecsolutions.com',
